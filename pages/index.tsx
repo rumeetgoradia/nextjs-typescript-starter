ac1586632eaca117de420e83df37a88eecc23001
Rumeet Goradia <rumeet.goradia@gmail.com>
2020-11-20 18:58:32 -0500

    Add SEO for index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import { NextSeo } from "next-seo"
+import Example from "../components/Example"
+import { SITE_NAME } from "../constants"
+
 const IndexPage = () => (
-  <>
-    <div>jsk world !</div>
-  </>
+	<>
+		<NextSeo titleTemplate="%s" title={SITE_NAME} />
+		<Example />
+	</>
 )
 
 export default IndexPage
