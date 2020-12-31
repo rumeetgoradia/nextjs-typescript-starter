996d269ce8adf032f665e957b56ca2602b1826a4
Rumeet Goradia <rumeet.goradia@gmail.com>
2020-12-31 16:41:04 -0500

    Fix next-optimized-images setup

--- next.config.js
+++ next.config.js
@@ -2,7 +2,40 @@ const path = require("path")
 const withPlugins = require("next-compose-plugins")
 const optimizedImages = require("next-optimized-images")
 
-module.exports = withPlugins([
-	[optimizedImages],
-	// other plugins here
-])
+const nextConfig = {
+	webpack: (config, { dev }) => {
+		config.resolve.alias["public"] = path.join(__dirname, "public")
+		config.resolve.alias["images"] = path.join(__dirname, "public", "images")
+		return config
+	},
+}
+
+module.exports = withPlugins(
+	[
+		[
+			optimizedImages,
+			{
+				/* config for next-optimized-images */
+			},
+		],
+		[
+			module,
+			{
+				rules: [
+					{
+						test: /\.(png|jpe?g|gif|svg|webp)$/i,
+						use: [
+							{
+								loader: "optimized-images-loader",
+								options: {},
+							},
+						],
+					},
+				],
+			},
+		],
+
+		// your other plugins here
+	],
+	nextConfig
+)
