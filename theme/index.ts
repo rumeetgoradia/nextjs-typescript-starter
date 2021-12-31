444b809370c55036f7fe63bb76200c35ce0a2f2e
Rumeet Goradia <rumeet.goradia@gmail.com>
2021-12-31 15:25:40 -0500

    Clean up

--- theme/index.ts
+++ theme/index.ts
@@ -1,5 +1,25 @@
 import { extendTheme } from "@chakra-ui/react"
 
+const fonts = [
+	"*",
+	"ui-sans-serif",
+	"system-ui",
+	"-apple-system",
+	"BlinkMacSystemFont",
+	"Segoe UI",
+	"Roboto",
+	"Helvetica Neue",
+	"Arial",
+	"Noto Sans",
+	"sans-serif",
+	"Apple Color Emoji",
+	"Segoe UI Emoji",
+	"Segoe UI Symbol",
+	"Noto Color Emoji",
+].join(",")
+
 const theme = extendTheme({})
 
 export default theme
+
+export { default as Fonts } from "./fonts"
--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,1 @@
-export * from "./opacity"
 export * from "./transition"
